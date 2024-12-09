c47a2767b67d20b32a68391203125bc66ee33596
Mike <Mike@192.168.0.8>
2024-12-09 20:57:36 +0530

    PROJECT SUPERMARKET

---
+++ 7martsupermarket/src/test/java/testscript/Base.java
@@ -0,0 +1,25 @@
+package testscript;
+
+
+	import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+	import java.time.Duration;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	public class Base {
+		public WebDriver driver;
+		@Test
+	 @BeforeMethod
+	 public void beforeMethod() {
+		  driver=new ChromeDriver();
+		  driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		  driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	  @AfterMethod
+	  public void afterMethod() {
+		 driver.quit();
+	  }
+	}
+
